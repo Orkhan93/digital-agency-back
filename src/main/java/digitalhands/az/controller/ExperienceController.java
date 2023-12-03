c7f20952ddc86dbcdcb8531b3b44f70d0d350262
OrxanMustafayev <68545038+Orkhan93@users.noreply.github.com>
2023-12-03 11:46:52 +0400

    Modified ExperienceService methods return type

--- src/main/java/digitalhands/az/controller/ExperienceController.java
+++ src/main/java/digitalhands/az/controller/ExperienceController.java
@@ -2,13 +2,12 @@ package digitalhands.az.controller;
 
 import digitalhands.az.request.ExperienceRequest;
 import digitalhands.az.response.ExperienceResponse;
+import digitalhands.az.response.ExperienceResponseList;
 import digitalhands.az.service.ExperienceService;
-import digitalhands.az.wrapper.ExperienceWrapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/experience")
@@ -20,23 +19,24 @@ public class ExperienceController {
     @PostMapping("/add/{userId}")
     public ResponseEntity<ExperienceResponse> createExperience(@RequestBody ExperienceRequest experienceRequest,
                                                                @PathVariable(name = "userId") Long userId) {
-        return experienceService.createExperience(experienceRequest, userId);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(experienceService.createExperience(experienceRequest, userId));
     }
 
     @PutMapping("/update/{userId}")
     public ResponseEntity<ExperienceResponse> updateExperience(@RequestBody ExperienceRequest experienceRequest,
                                                                @PathVariable Long userId) {
-        return experienceService.updateExperience(experienceRequest, userId);
+        return ResponseEntity.status(HttpStatus.OK).body(experienceService.updateExperience(experienceRequest, userId));
     }
 
     @GetMapping("/get/{experienceId}")
     public ResponseEntity<ExperienceResponse> getExperienceById(@PathVariable(name = "experienceId") Long experienceId) {
-        return experienceService.getExperienceById(experienceId);
+        return ResponseEntity.status(HttpStatus.OK).body(experienceService.getExperienceById(experienceId));
     }
 
     @GetMapping("/getAll")
-    public ResponseEntity<List<ExperienceWrapper>> getAllExperiences() {
-        return experienceService.getAllExperiences();
+    public ResponseEntity<ExperienceResponseList> getAllExperiences() {
+        return ResponseEntity.status(HttpStatus.OK).body(experienceService.getAllExperiences());
     }
 
     @DeleteMapping("/{userId}/delete/{experienceId}")
