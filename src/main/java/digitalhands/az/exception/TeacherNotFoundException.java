317f445d7620275307e81e29c77e3badcc894236
OrxanMustafayev <68545038+Orkhan93@users.noreply.github.com>
2023-12-07 16:48:44 +0400

    Modified TeacherService methods return type

--- src/main/java/digitalhands/az/exception/TeacherNotFoundException.java
+++ src/main/java/digitalhands/az/exception/TeacherNotFoundException.java
@@ -2,7 +2,7 @@ package digitalhands.az.exception;
 
 public class TeacherNotFoundException extends RuntimeException {
 
-    public TeacherNotFoundException(String message) {
+    public TeacherNotFoundException(String code, String message) {
         super(message);
     }
 
